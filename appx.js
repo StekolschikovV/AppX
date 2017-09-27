e62455a029a52ad68604c34d816812445079c242
stekolschikov <stekolschikov@mail.ua>
2017-09-27 15:44:34 +0300

    ptf

--- appx.js
+++ appx.js
@@ -2,7 +2,7 @@ const convertToWindowsStore = require('electron-windows-store')
 
 convertToWindowsStore({
    containerVirtualization: false,
-   inputDirectory: 'C:\\Users\\\steko\\Desktop\\AppX\\Brain++\\',
+   inputDirectory: 'C:\\Users\\\steko\\Desktop\\AppX\\Brain-plus-plus\\',
    outputDirectory: 'C:\\Users\\\steko\\Desktop\\AppX\\output\\',
    flatten: false,
    packageVersion: '1.0.0.0',
@@ -15,9 +15,9 @@ convertToWindowsStore({
    deploy: false,
    publisher: 'CN=developmentca',
    windowsKit: 'C:\\Program Files (x86)\\Windows Kits\\10\\bin\\x64 ',
-   //devCert: 'C:\\devcert.pfx',
-   //certPass: 'abcd',
-   //desktopConverter: 'C:\\Tools\\DesktopConverter',
+   devCert: 'C:\\Users\\\steko\\Desktop\\AppX\\devcert.pfx',
+   certPass: '123456',
+   desktopConverter: 'C:\\desktop-converter-tools',
    expandedBaseImage: 'C:\\BaseImage.wim', // BaseImage-16257.wim https://www.microsoft.com/en-us/download/details.aspx 
    makeappxParams: ['/l'],
    signtoolParams: ['/p'],
